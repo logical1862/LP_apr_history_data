7e2323d5ff248aca7694d05f08bdd05728e99400
logical1862 <100737462+logical1862@users.noreply.github.com>
2022-07-03 12:45:51 -0500

    html dropdown / sql database connection
    added sqlite3 connection to pool_apr.db in app.py. Will use queries to generate plot data.
    
    added dropdown on index.html that lists names of all tables in database. each table represents one liquidity pools historical apr reward data

--- dashboard_site/app.py
+++ dashboard_site/app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template
+import sqlite3
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    with sqlite3.connect('../pool_apr.db') as conn:
+       cur = conn.cursor()
+       cur.execute("""
+SELECT name FROM sqlite_schema
+WHERE type='table'
+ORDER BY name;
+                """)
+       name_list = cur.fetchall()
+    return render_template('index.html', name_list=name_list)
