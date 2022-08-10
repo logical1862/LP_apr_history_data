a012e3dd4f6c459bc792bbb6e28d7c68159e98c0
logical1862 <100737462+logical1862@users.noreply.github.com>
2022-08-09 22:12:58 -0500

    feature: create / update pool_apr.db
    add functions (application.py):
    
    -update_apr_db()
    ----removes current database and downloads current s3 version in its place.
    ----uses wget() to download object from public url
    
    -apr_db_needs_update()
    ----uses datetime module to check age of current database
    ----if older than 24hrs, returns true
    
    -apr_database_check()
    ----runs update_apr_db() if needed
    
    -get_name_list()
    ----executes sql query for name list passed to render_template.
    ----separated as part of restructure to allow for database functions

--- dashboard_site/application.py
+++ dashboard_site/application.py
@@ -1,9 +1,64 @@
 from flask import Flask, render_template, request, flash
+from datetime import datetime
 import sqlite3
 import pandas as pd
+import wget
+import os
 
 import helpers
-import os
+
+
+def update_apr_db():
+   """Removes current pool_apr.db and replaces with the most current database from S3"""
+   
+   os.remove('pool_apr.db')
+
+   database_s3_url = 'https://osmosystemdata.s3.amazonaws.com/poolAPRdatabase/pool_apr.db'
+
+   database_local_path = 'pool_apr.db'
+
+   try:
+      wget.download(database_s3_url, out=database_local_path)
+
+   except Exception as e:
+      pass
+
+
+def apr_db_needs_update():
+
+   # both times are in UNIX timestamp format
+
+   time_of_last_update = os.path.getctime('pool_apr.db')
+
+   time_now = datetime.now().timestamp()
+
+   # function returns seconds since creation, 86400 seconds in a day
+   if (time_now - time_of_last_update) > 86400:
+      return True
+
+   return False
+
+
+
+def apr_database_check():
+   if apr_db_needs_update():
+      update_apr_db()
+
+   else:
+      pass
+
+def get_name_list():
+    # create cursor connection to database, get list of table names
+      conn = sqlite3.connect('pool_apr.db')
+      cur = conn.cursor()
+      cur.execute("""
+   SELECT name FROM sqlite_schema
+   WHERE type='table'
+   ORDER BY name;
+                  """)
+      name_list = cur.fetchall()
+      return name_list, conn
+
 
 app = Flask(__name__)
 
@@ -12,26 +67,29 @@ app.secret_key='SECRET'
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-   # create cursor connection to database, get list of table names
-   conn = sqlite3.connect('pool_apr.db')
-   cur = conn.cursor()
-   cur.execute("""
-SELECT name FROM sqlite_schema
-WHERE type='table'
-ORDER BY name;
-                """)
-   name_list = cur.fetchall()
+
+   apr_database_check()
+
+
+   if request.method == 'GET':
+      # create cursor connection to database, get list of table names
+      name_list, conn = get_name_list()
 
 
 
-    # first plot img shown on site load
+      # first plot img shown on site load
 
-    # TODO dynamically set first img to first pool name in list, file path should be '\static\plot_imgs\{< first pool >}'
+      # TODO dynamically set first img to first pool name in list, file path should be '\static\plot_imgs\{< first pool >}'
 
-   initial_img_path = R"/static/function_test_sample_files/AKT_ATOM.png"
+      initial_img_path = R"/static/function_test_sample_files/AKT_ATOM.png"
+
+      return render_template('index.html', name_list=name_list, img_path=initial_img_path)
+
+
 
     # post should happen when submit button is clicked to view different pool plots
    if request.method == 'POST':
+      name_list, conn = get_name_list()
 
       # get name of selected pool
       pool_plot_img_selection = request.form.get('pool_selection')
@@ -51,4 +109,4 @@ ORDER BY name;
       flash(f'Selected: {pool_plot_img_selection}', 'pool_selection')
       return render_template('index.html', name_list=name_list, img_path=img_path)
    # page first load
-   return render_template('index.html', name_list=name_list, img_path=initial_img_path)
+
