58b4deb8a6983bcd3a73a7d6c00e897cf127b6ea
logical1862 <100737462+logical1862@users.noreply.github.com>
2022-08-10 21:48:27 -0500

    move functions
    move [update_apr_db(), apr_db_needs_update(), apr_database_check(), get_name_list()] to helpers.py

--- dashboard_site/application.py
+++ dashboard_site/application.py
@@ -1,63 +1,9 @@
 from flask import Flask, render_template, request, flash
-from datetime import datetime
-import sqlite3
 import pandas as pd
-import wget
-import os
+
+
 
 import helpers
-
-
-def update_apr_db():
-   """Removes current pool_apr.db and replaces with the most current database from S3"""
-   
-   os.remove('pool_apr.db')
-
-   database_s3_url = 'https://osmosystemdata.s3.amazonaws.com/poolAPRdatabase/pool_apr.db'
-
-   database_local_path = 'pool_apr.db'
-
-   try:
-      wget.download(database_s3_url, out=database_local_path)
-
-   except Exception as e:
-      pass
-
-
-def apr_db_needs_update():
-
-   # both times are in UNIX timestamp format
-
-   time_of_last_update = os.path.getctime('pool_apr.db')
-
-   time_now = datetime.now().timestamp()
-
-   # function returns seconds since creation, 86400 seconds in a day
-   if (time_now - time_of_last_update) > 86400:
-      return True
-
-   return False
-
-
-
-def apr_database_check():
-   if apr_db_needs_update():
-      update_apr_db()
-
-   else:
-      pass
-
-def get_name_list():
-    # create cursor connection to database, get list of table names
-      conn = sqlite3.connect('pool_apr.db')
-      cur = conn.cursor()
-      cur.execute("""
-   SELECT name FROM sqlite_schema
-   WHERE type='table'
-   ORDER BY name;
-                  """)
-      name_list = cur.fetchall()
-      return name_list, conn
 
 
 app = Flask(__name__)
@@ -68,12 +14,12 @@ app.secret_key='SECRET'
 def index():
 
 
-   apr_database_check()
+   helpers.apr_database_check()
 
 
    if request.method == 'GET':
       # create cursor connection to database, get list of table names
-      name_list, conn = get_name_list()
+      name_list, conn = helpers.get_name_list()
 
 
 
@@ -89,7 +35,7 @@ def index():
 
     # post should happen when submit button is clicked to view different pool plots
    if request.method == 'POST':
-      name_list, conn = get_name_list()
+      name_list, conn = helpers.get_name_list()
 
       # get name of selected pool
       pool_plot_img_selection = request.form.get('pool_selection')
--- dashboard_site/helpers.py
+++ dashboard_site/helpers.py
@@ -1,8 +1,13 @@
 # functions for site
 
+import wget
+import os
+import sqlite3
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+from datetime import datetime
+
 
 def save_plot_image(df:pd.DataFrame, file_name:str):
     
@@ -24,6 +29,64 @@ def save_plot_image(df:pd.DataFrame, file_name:str):
 
     #remove excess whitespace around image and save
     plt.savefig('{}{}'.format(plot_folder, file_name), bbox_inches='tight')
+
+
+
+def update_apr_db():
+   """Removes current pool_apr.db and replaces with the most current database from S3"""
+   
+   os.remove('pool_apr.db')
+
+   database_s3_url = 'https://osmosystemdata.s3.amazonaws.com/poolAPRdatabase/pool_apr.db'
+
+   database_local_path = 'pool_apr.db'
+
+   try:
+      wget.download(database_s3_url, out=database_local_path)
+
+   except Exception as e:
+      pass
+
+
+def apr_db_needs_update():
+
+   # both times are in UNIX timestamp format
+
+   time_of_last_update = os.path.getctime('pool_apr.db')
+
+   time_now = datetime.now().timestamp()
+
+   # function returns seconds since creation, 86400 seconds in a day
+   if (time_now - time_of_last_update) > 86400:
+      return True
+
+   return False
+
+
+
+def apr_database_check():
+   if apr_db_needs_update():
+      update_apr_db()
+
+   else:
+      pass
+
+def get_name_list():
+    # create cursor connection to database, get list of table names
+      conn = sqlite3.connect('pool_apr.db')
+      cur = conn.cursor()
+      cur.execute("""
+   SELECT name FROM sqlite_schema
+   WHERE type='table'
+   ORDER BY name;
+                  """)
+      name_list = cur.fetchall()
+      return name_list, conn
+
+
+
+
+
 
 
 if __name__ == '__main__':
