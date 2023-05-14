9157920847ab5f73251221109a6470fc101f61c7
logical1862 <brophy93@yahoo.com>
2023-05-13 22:15:01 -0500

    Format/refactor
    Used pylint to clean .py scripts
    reduced code in application.py from separate code blocks for get/post requests to a single block

--- dashboard_site/application.py
+++ dashboard_site/application.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, flash
 import pandas as pd
 
@@ -9,50 +10,27 @@ import helpers
 app = Flask(__name__)
 
 app.secret_key='SECRET'
+print(os.getcwd)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
 
-   helpers.apr_database_check()
+    helpers.apr_database_check()
+    name_list, conn = helpers.get_name_list()
 
+    # get name of selected pool
+    pool_plot_img_selection = request.form.get('pool_selection')
 
-   if request.method == 'GET':
-      # create cursor connection to database, get list of table names
-      name_list, conn = helpers.get_name_list()
+    # generate new plot
+    selected_pool_df = pd.read_sql(
+        f'SELECT * FROM {pool_plot_img_selection}',
+        conn,
+        index_col='index'
+        )
+    helpers.save_plot_image(selected_pool_df, pool_plot_img_selection)
+    img_path = f"static/img_files/{pool_plot_img_selection}.png"
 
+    flash(f'Selected: {pool_plot_img_selection}', 'pool_selection')
 
-
-      # first plot img shown on site load
-
-      # TODO dynamically set first img to first pool name in list, file path should be '\static\plot_imgs\{< first pool >}'
-
-      initial_img_path = R"/static/function_test_sample_files/AKT_ATOM.png"
-
-      return render_template('index.html', name_list=name_list, img_path=initial_img_path)
-
-
-
-    # post should happen when submit button is clicked to view different pool plots
-   if request.method == 'POST':
-      name_list, conn = helpers.get_name_list()
-
-      # get name of selected pool
-      pool_plot_img_selection = request.form.get('pool_selection')
-
-      # create dataframe by reading pool_apr.db,with name of pool pass to helpers.save_plot_image(overwriting if it exists)
-      # before calling on that image to return index template with selected pool as new plot image
-
-      selected_pool_df = pd.read_sql(f'SELECT * FROM {pool_plot_img_selection}', conn, index_col='index')
-
-      helpers.save_plot_image(selected_pool_df, pool_plot_img_selection)
-
-
-      # TODO set path to selected pool in '\static\plot_imgs\'
-
-      img_path = "/static/plot_imgs/{}.png".format(pool_plot_img_selection)
-
-      flash(f'Selected: {pool_plot_img_selection}', 'pool_selection')
-      return render_template('index.html', name_list=name_list, img_path=img_path)
-   # page first load
-
+    return render_template('index.html', name_list=name_list, img_path=img_path)
--- dashboard_site/helpers.py
+++ dashboard_site/helpers.py
@@ -1,31 +1,32 @@
-# functions for site
+"""helpers for application.py"""
 
-import wget
 import os
 import sqlite3
+from datetime import datetime
+
+import wget
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
-from datetime import datetime
 
 
 def save_plot_image(df:pd.DataFrame, file_name:str):
-    
+
     sns.set_style('darkgrid')
     plt.figure(figsize=(15, 10))
-    
+
     plot = sns.lineplot(data=df)
-    
+
     plot.set_xticks(range(len(df.day)))
     plot.set_xticklabels(df['day'])
 
     plt.xlabel('Date of Record')
     plt.ylabel('Pool Reward Annual Percentage Rate') 
     plt.xticks(rotation=45)
-    
+
     # test_plot_folder = 'static\\function_test_sample_files\\'
 
-    plot_folder = 'static/plot_imgs/'
+    plot_folder = 'static/img_files/'
 
     #remove excess whitespace around image and save
     plt.savefig('{}{}'.format(plot_folder, file_name), bbox_inches='tight')
@@ -33,59 +34,55 @@ def save_plot_image(df:pd.DataFrame, file_name:str):
 
 
 def update_apr_db():
-   """Removes current pool_apr.db and replaces with the most current database from S3"""
-   
-   os.remove('pool_apr.db')
+    """Removes current pool_apr.db and replaces with the most current database from S3"""
 
-   database_s3_url = 'https://osmosystemdata.s3.amazonaws.com/poolAPRdatabase/pool_apr.db'
+    os.remove('../pool_apr.db')
 
-   database_local_path = 'pool_apr.db'
+    database_s3_url = 'https://osmosystemdata.s3.amazonaws.com/poolAPRdatabase/pool_apr.db'
 
-   try:
-      wget.download(database_s3_url, out=database_local_path)
+    database_local_path = '../pool_apr.db'
 
-   except Exception as e:
-      pass
+    try:
+        wget.download(database_s3_url, out=database_local_path)
+
+    except Exception as e:
+       pass
 
 
 def apr_db_needs_update():
 
    # both times are in UNIX timestamp format
 
-   time_of_last_update = os.path.getctime('pool_apr.db')
+    time_of_last_update = os.path.getctime('../pool_apr.db')
 
-   time_now = datetime.now().timestamp()
+    time_now = datetime.now().timestamp()
 
-   # function returns seconds since creation, 86400 seconds in a day
-   if (time_now - time_of_last_update) > 86400:
-      return True
+    # function returns seconds since creation, 86400 seconds in a day
+    if (time_now - time_of_last_update) > 86400:
+        return True
 
-   return False
+    return False
 
 
 
 def apr_database_check():
-   if apr_db_needs_update():
-      update_apr_db()
+    if apr_db_needs_update():
+        update_apr_db()
 
-   else:
-      pass
+    else:
+        pass
 
 def get_name_list():
     # create cursor connection to database, get list of table names
-      conn = sqlite3.connect('pool_apr.db')
-      cur = conn.cursor()
-      cur.execute("""
-   SELECT name FROM sqlite_schema
-   WHERE type='table'
-   ORDER BY name;
+    conn = sqlite3.connect('../pool_apr.db')
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT name FROM sqlite_schema
+        WHERE type='table'
+        ORDER BY name;
                   """)
-      name_list = cur.fetchall()
-      return name_list, conn
-
-
-
-
+    name_list = cur.fetchall()
+    return name_list, conn
 
 
 
@@ -94,5 +91,5 @@ if __name__ == '__main__':
     csv_path = R'dashboard_site/static/function_test_sample_files/AKT_OSMO.csv'
     file_name = csv_path[49:-3]
     df = pd.read_csv(csv_path)
-    
+
     save_plot_image(df, file_name)
